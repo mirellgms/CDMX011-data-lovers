6de6c1c7adbcebfce90727725c122645b5a6c34c
karen-gup <ana.kgp25@gmail.com>
2021-07-27 12:07:57 -0500

    Cambios en data.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,29 @@
-// estas funciones son de ejemplo
+import data from './data/athletes/athletes.js';
+export const names = data.athletes
+  names.sort((a,b) => {
+  if(a.name > b.name){
+    return 1;
+  } else if (a.name < b.name){
+    return -1;
+  }
+    return 0;
+});
 
-export const example = () => {
-  return 'example';
-};
+export let gold = names.filter((name)=>{
+  if(name.medal=== "Gold"){
+    return name;
+  }
+})
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export let country = names.filter((name)=>{
+  if(name.team=== "Algeria"){
+    return name;
+  }
+})
+
+export function byTeam(pais){
+  let filterPerTeam=data.athletes.filter(function(team) {
+  return team.team == pais;
+})
+return filterPerTeam
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { example } from './data.js';
+/*import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+console.log(example, data);*/
+import {filterPerTeam} from "./data.js";
 
-console.log(example, data);
+const btn= document.getElementById("btn");
+    btn.addEventListener("click", function(){
+        document.getElementById("central").style.display="none";
+        document.getElementById("filter").style.display="block";
+       console.log (filterPerTeam);
+    })
