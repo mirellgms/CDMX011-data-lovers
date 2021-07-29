e2885344c3fe4ec50acc3c035eb500a7326ef4fd
mirellgms <86274232+mirellgms@users.noreply.github.com>
2021-07-28 23:31:06 -0500

    Mostar objeto team

--- src/data.js
+++ src/data.js
@@ -1,9 +1,29 @@
 // estas funciones son de ejemplo
 
+import athletes from './data/athletes/athletes.js';
+
+let teams = [];
+for (let i = 0; i < athletes.athletes.length; i++){
+	teams.push(athletes.athletes[i].team);
+  //teams.push(athletes.athletes[i].name); 
+  //teams.push(athletes.athletes[i].noc);
+  //teams.push(athletes.athletes[i].age);
+  //teams.push(athletes.athletes[i].event);
+  //teams.push(athletes.athletes[i].medal); 
+}
+teams.sort();
+const set = new Set(teams);
+Array.from(Set)
+
+  export {teams,set};
+
+
+
+/*
 export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+}; */
--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
-import data from './data/athletes/athletes.js';
-console.log(data);
+
+
+import {teams,set} from './data.js';
+console.log(set);
+const btn = document.getElementById("btn")
+btn.addEventListener("click", function () {
+    document.getElementById("filter").style.display = "block";
+    document.getElementById("central").style.display = "none";
+    document.getElementById("show").style.display = "block";
+    document.getElementById("show").innerHTML = Array.from(set);
+    
+});
+
+
+
+
+//import {example, anotherExample}  from "./data.js"  
+//console.log(example);
