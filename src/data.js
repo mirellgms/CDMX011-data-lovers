39e9ef87646002bf344c60423942e48f59244b9b
karen-gup <ana.kgp25@gmail.com>
2021-08-03 23:16:39 -0500

    Mostrar data

--- src/data.js
+++ src/data.js
@@ -1,29 +1,47 @@
 import data from './data/athletes/athletes.js';
-
 export const names = data.athletes
-  names.sort((a,b) => {
-  if(a.name > b.name){
-    return 1;
-  } else if (a.name < b.name){
-    return -1;
-  }
-    return 0;
-});
-
+names.sort((a, b) => {
+  if (a.name > b.name) {
+  return 1;
+  } else if (a.name < b.name) {
+  return -1;
+    }
+  return 0;
+  });
+export function filterByTeam(countries) {
+  let dataByTeam = data.athletes.filter(function (team) {
+    if (team.team == countries) {
+    return team;
+    }
+  })
+  return dataByTeam;
+}
+export function filterByMedall(medalla){
+  let dataByMedal= data.athletes.filter(function(medal){
+    if(medal.medal== medalla){
+    return medal;
+    }
+  })
+  return dataByMedal;
+}
+export function filterBySport(deporte){
+  let dataBySport= data.athletes.filter(function(sport){
+    if(sport.sport== deporte){
+    return sport;
+    }
+  })
+  return dataBySport;
+}
+/*
 export let gold = names.filter((name)=>{
   if(name.medal=== "Gold"){
     return name;
   }
-})
-export function byTeam(){
-  let teams=names
-  for(let i=0; i<teams.length; i++) {
-  console.log(teams[i].team);
-}}
+});
 
-
-export let country = names.filter((name)=>{
-  if(name.team=== "Algeria"){
-    return name;
+export let country = names.filter((team)=>{
+  if(team.team=== "Algeria"){
+    return team;
   }
 })
+*/
--- src/main.js
+++ src/main.js
@@ -1,2 +1,70 @@
 import data from './data/athletes/athletes.js';
-console.log(data);
+import { filterByTeam } from './data.js';
+//Toma el id="root" del html para guardar la funcion de mostrarData
+const root = document.getElementById("root");
+data.athletes.forEach(athletas => {
+    let atleta = mostrarData(athletas)
+    root.appendChild(atleta);
+});
+//Muestra toda la data en interfaz
+function mostrarData(athleta) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    const names = document.createElement("p");
+    names.textContent = `Nombre: ${athleta.name.toUpperCase()}`
+    const deporte = document.createElement("p");
+    deporte.textContent = `Deporte: ${athleta.sport}`
+    const evento = document.createElement("p");
+    evento.textContent = `Evento: ${athleta.event}`
+    const medalla = document.createElement("p");
+    medalla.textContent = `Medalla: ${athleta.medal.toUpperCase()}`
+    const pais = document.createElement("p");
+    pais.textContent = `Pais: ${athleta.noc}`
+    const edad = document.createElement("p");
+    edad.textContent = `Edad: ${athleta.age} años`
+    const genero = document.createElement("p");
+    genero.textContent = `Genero: ${athleta.gender}`
+    const altura = document.createElement("p");
+    altura.textContent = `Estatura: ${athleta.height} cm`
+    const peso = document.createElement("p");
+    peso.textContent = `Peso: ${athleta.weight} kg`
+    card.appendChild(names);
+    card.appendChild(deporte);
+    card.appendChild(evento);
+    card.appendChild(pais);
+    card.appendChild(medalla);
+    card.appendChild(edad);
+    card.appendChild(genero);
+    card.appendChild(altura);
+    card.appendChild(peso);
+    return card;
+}
+//Remueve el nodo que contiene la funcion que muestra toda la data
+function clear() {
+    while (root.firstChild) {
+        root.removeChild(root.firstChild)
+    }
+}
+//Filtro por pais
+const selectByTeam = document.getElementById("countries");
+selectByTeam.addEventListener("change", function (e) {
+    clear()
+    console.log(filterByTeam(e.target.value));
+});
+
+/*
+function generadordeHTML(unsolodato){
+    let titulito = document.createElement("p");
+    titulito.textContent=`Pais: ${unsolodato.team}`
+    return titulito
+}
+const root= document.getElementById("root");
+   data.athletes.forEach(unoporunodelosjugadores => {
+       let jugadorcito=generadordeHTML(unoporunodelosjugadores)
+       root.appendChild(jugadorcito)
+   });
+selectPais.addEventListener("change",function(e){
+  let filterData= paisito(e.target.value);
+  paises = Array.from(s);
+          document.getElementById('paises').innerHTML ='<li>' + paises.join('</li><li>') + '</li>'
+*/
