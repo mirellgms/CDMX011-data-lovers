98a79b38f506a1a4f5f6cfb838b2c8c34ff4fbb6
mirellgms <86274232+mirellgms@users.noreply.github.com>
2021-08-17 00:17:38 -0500

    Cambios dataMock

--- test/dataMock.js
+++ test/dataMock.js
@@ -1,43 +1,28 @@
 export const mockData= [
-      {
-        "name": "Nicola Virginia Adams",
-        "gender": "F",
-        "height": "164",
-        "weight": "51",
-        "sport": "Boxing",
-        "team": "Great Britain",
-        "noc": "GBR",
-        "age": 33,
-        "event": "Boxing Women's Flyweight",
-        "medal": "Gold"
-      },
-      {
-        "name": "Luc Abalo",
-        "gender": "Masculino",
-        "height": "182",
-        "weight": "86",
-        "sport": "Handball",
-        "team": "France",
-        "noc": "FRA",
-        "age": 31,
-        "event": "Handball Men's Handball",
-        "medal": "Silver"
-      },
-      {
-        "name": "Aaron Brown",
-        "gender": "M",
-        "height": "198",
-        "weight": "79",
-        "sport": "Athletics",
-        "team": "Canada",
-        "noc": "CAN",
-        "age": 24,
-        "event": "Athletics Men's 4 x 100 metres Relay",
-        "medal": "Bronze"
-      }
-    ];
-
-export const athletA= [
+  {
+    "name": "Nicola Virginia Adams",
+    "gender": "F",
+    "height": "164",
+    "weight": "51",
+    "sport": "Boxing",
+    "team": "Great Britain",
+    "noc": "GBR",
+    "age": 33,
+    "event": "Boxing Women's Flyweight",
+    "medal": "Gold"
+  },
+  {
+    "name": "Luc Abalo",
+    "gender": "Masculino",
+    "height": "182",
+    "weight": "86",
+    "sport": "Handball",
+    "team": "France",
+    "noc": "FRA",
+    "age": 31,
+    "event": "Handball Men's Handball",
+    "medal": "Silver"
+  },
   {
     "name": "Aaron Brown",
     "gender": "M",
@@ -51,31 +36,46 @@ export const athletA= [
     "medal": "Bronze"
   }
 ];
+
+export const athletA= [
+{
+"name": "Aaron Brown",
+"gender": "M",
+"height": "198",
+"weight": "79",
+"sport": "Athletics",
+"team": "Canada",
+"noc": "CAN",
+"age": 24,
+"event": "Athletics Men's 4 x 100 metres Relay",
+"medal": "Bronze"
+}
+];
 export const athletL= [
-   {
-    "name": "Luc Abalo",
-    "gender": "Masculino",
-    "height": "182",
-    "weight": "86",
-    "sport": "Handball",
-    "team": "France",
-    "noc": "FRA",
-    "age": 31,
-    "event": "Handball Men's Handball",
-    "medal": "Silver"
-  }
+{
+"name": "Luc Abalo",
+"gender": "Masculino",
+"height": "182",
+"weight": "86",
+"sport": "Handball",
+"team": "France",
+"noc": "FRA",
+"age": 31,
+"event": "Handball Men's Handball",
+"medal": "Silver"
+}
 ];
 export const athletN= [
-  {
-    "name": "Nicola Virginia Adams",
-    "gender": "F",
-    "height": "164",
-    "weight": "51",
-    "sport": "Boxing",
-    "team": "Great Britain",
-    "noc": "GBR",
-    "age": 33,
-    "event": "Boxing Women's Flyweight",
-    "medal": "Gold"
-  }
+{
+"name": "Nicola Virginia Adams",
+"gender": "F",
+"height": "164",
+"weight": "51",
+"sport": "Boxing",
+"team": "Great Britain",
+"noc": "GBR",
+"age": 33,
+"event": "Boxing Women's Flyweight",
+"medal": "Gold"
+}
 ];
